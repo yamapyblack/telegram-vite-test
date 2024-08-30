0edded6434114c3d54017038a14267698a42f279
yamapyblack <s25050730r@gmail.com>
2024-08-30 16:19:45 +0900

    add @twa-dev/sdk

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import WebApp from "@twa-dev/sdk";
 
-createRoot(document.getElementById('root')!).render(
+WebApp.ready();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
